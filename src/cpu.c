e3ae44d48bd02dccf0d1bf5ec6fe19e5fcb164fe
zcarp <zcarp7220@gmail.com>
2025-02-17 15:04:26 -0600

    Autoformatting!

--- src/cpu.c
+++ src/cpu.c
@@ -1,8 +1,8 @@
 #include "functions.h"
-#define secondNibbleMask 0b0000111100000000
-#define thirdNibbleMask  0b0000000011110000
-#define fourthNibbleMask 0b0000000000001111
-#define secondByteMask   0b0000000011111111
+#define secondNibbleMask 0x0F00
+#define thirdNibbleMask 0x00F0
+#define fourthNibbleMask 0x000F
+#define secondByteMask 0x00FF
 u_int16_t vI = 0;
 u_int16_t stack[16];
 u_int8_t SP = 0;
@@ -13,184 +13,186 @@ int ST = 0;
 int skipInstructionIf(bool check);
 bool keyboardCopy;
 bool vF;
-int cpuStep(u_int16_t cI){
-    if(ST != 0){
-	ST--;
-    }
-    if(DT != 0){
-	DT--;
-    }
+int cpuStep(u_int16_t cI) {
+  if (ST != 0) {
+    ST--;
+  }
+  if (DT != 0) {
+    DT--;
+  }
   int secondNibble = (cI & secondNibbleMask) >> 8;
-  int thirdNibble  = (cI & thirdNibbleMask) >> 4;
+  int thirdNibble = (cI & thirdNibbleMask) >> 4;
   int fourthNibble = (cI & fourthNibbleMask);
-u_int8_t * vX = &registers[secondNibble];
-	u_int8_t * vY = &registers[thirdNibble];
+  u_int8_t *vX = &registers[secondNibble];
+  u_int8_t *vY = &registers[thirdNibble];
   int byte2 = cI & secondByteMask;
-  switch (cI & 0xF000){
-    case 0x1000:
+  switch (cI & 0xF000) {
+  case 0x1000:
     return (cI - 0x1000) - PC;
-    case 0x6000:
+  case 0x6000:
     registers[secondNibble] = byte2;
     return 2;
-    case 0x7000:
+  case 0x7000:
     registers[secondNibble] = byte2 + registers[secondNibble];
     return 2;
-    case 0xA000:
+  case 0xA000:
     vI = cI & 0x0FFF;
     return 2;
-    case 0xD000:
-    for(int i = 0; i < fourthNibble; i++){
+  case 0xD000:
+    for (int i = 0; i < fourthNibble; i++) {
       tmp = memory[vI + i];
-      for(int k = 0; k < 8; k++){	
-        vF = screen[registers[secondNibble] + k][registers[thirdNibble] + i]; 
-        screen[registers[secondNibble] + k][registers[thirdNibble] + i] ^= tmp >> (7-k) & 1;
-	if(screen[registers[secondNibble] + k][registers[thirdNibble] + i] && !vF ){registers[0xF] = 1;}
+      for (int k = 0; k < 8; k++) {
+        vF = screen[registers[secondNibble] + k][registers[thirdNibble] + i];
+        screen[registers[secondNibble] + k][registers[thirdNibble] + i] ^=
+            tmp >> (7 - k) & 1;
+        if (screen[registers[secondNibble] + k][registers[thirdNibble] + i] &&
+            !vF) {
+          registers[0xF] = 1;
+        }
       }
-
     }
     return 2;
-    case 0x3000:
+  case 0x3000:
     return skipInstructionIf(registers[secondNibble] == byte2);
-    case 0x4000:
+  case 0x4000:
     return skipInstructionIf(registers[secondNibble] != byte2);
-    case 0x5000:
+  case 0x5000:
     return skipInstructionIf(registers[secondNibble] == registers[thirdNibble]);
-    case 0x9000:
+  case 0x9000:
     return skipInstructionIf(registers[secondNibble] != registers[thirdNibble]);
-    case 0x2000:
+  case 0x2000:
     SP++;
     stack[SP] = PC;
     PC = cI & 0b0000111111111111;
     return 0;
-    case 0xC000:
+  case 0xC000:
     registers[secondNibble] = (rand() % 255) && byte2;
     return 2;
-    case 0x8000:
-	vF = registers[0xF];
-    	switch (fourthNibble){
-		case 0:
-		*vX = *vY;
-		return 2;
-		case 1:
-		*vX = *vX | *vY;
-		return 2;
-		case 2:
-		*vX = *vX & *vY;
-		return 2;
-		case 3:
-		*vX = *vX ^ *vY;
-		return 2;
-		case 4:
-		if(*vX + *vY < 255){
-		vF = !vF;
-		}
-		*vX += *vY;
-		registers[0xF] = vF;
-		return 2;
-		case 5:
-		*vX -= *vY;
-		if(*vX > *vY){
-		registers[0x0F] = !registers[0x0F];
-		}
-		return 2;	
-		case 6:
-		vF = *vX & 1;
-		*vX >>= 1;
-		registers[0xF] = vF;
-		return 2;
-		case 7:
-		vF = *vY > *vX;
-		*vX = *vY - *vX;
-		registers[0xF] = vF;
-		return 2;
-		case 0x0E:
-		vF = *vX & 128;
-		*vX = *vX << 1;
-		registers[0xF] = vF;
-		return 2;
-	    }
-    case 0xF000:
-	vX = &registers[secondNibble];
-	switch(byte2){
-	case 0x65:
-	for(int i = 0; i < secondNibble + 1; i++){
-		registers[i] = memory[i + vI]; 
-	}
-	return 2;
-	case 0x55:
-	for(int i = 0; i < secondNibble + 1; i++){
-		memory[i + vI] = registers[i]; 
-	}
-	return 2;
-	case 0x33:
-	memory[vI] = *vX/100 % 10;
-	memory[vI + 1] = *vX/10 % 10;
-	memory[vI + 2] = *vX % 10;
-	return 2;
-	case 0x1E:
-	vI = *vX + vI;
-	return 2;
-	case 0x15:
-	DT = *vX;
-	return 2;
-	case 0x18:
-	ST = *vX;
-	return 2;
-	case 0x07:
-	*vX = DT;
-	return 2;
-	case 0x29:
-	vI = *vX * 5;
-	return 2;
-	case 0xA:
-	keyboardCopy = false;
-	for(int i = 0; i < 16; i++){
-	if(keyboard[i]){
-	if(keyboardCopy != keyboard[i]){
-	*vX = i;
-	return 2;
-	}
-	keyboardCopy = keyboard[i];
-	}
-	}
-	return 0;
-	}
-    case 0xE000:
-	vX = &registers[secondNibble];
-	switch(byte2){
-	case 0x9E:
-	return skipInstructionIf(keyboard[*vX] == true);
-	case 0xA1:
-	return skipInstructionIf(keyboard[*vX] == false);
-	}
+  case 0x8000:
+    vF = registers[0xF];
+    switch (fourthNibble) {
+    case 0:
+      *vX = *vY;
+      return 2;
+    case 1:
+      *vX = *vX | *vY;
+      return 2;
+    case 2:
+      *vX = *vX & *vY;
+      return 2;
+    case 3:
+      *vX = *vX ^ *vY;
+      return 2;
+    case 4:
+      if (*vX + *vY < 255) {
+        vF = !vF;
+      }
+      *vX += *vY;
+      registers[0xF] = vF;
+      return 2;
+    case 5:
+      *vX -= *vY;
+      if (*vX > *vY) {
+        registers[0x0F] = !registers[0x0F];
+      }
+      return 2;
+    case 6:
+      vF = *vX & 1;
+      *vX >>= 1;
+      registers[0xF] = vF;
+      return 2;
+    case 7:
+      vF = *vY > *vX;
+      *vX = *vY - *vX;
+      registers[0xF] = vF;
+      return 2;
+    case 0x0E:
+      vF = *vX & 128;
+      *vX = *vX << 1;
+      registers[0xF] = vF;
+      return 2;
+    }
+  case 0xF000:
+    vX = &registers[secondNibble];
+    switch (byte2) {
+    case 0x65:
+      for (int i = 0; i < secondNibble + 1; i++) {
+        registers[i] = memory[i + vI];
+      }
+      return 2;
+    case 0x55:
+      for (int i = 0; i < secondNibble + 1; i++) {
+        memory[i + vI] = registers[i];
+      }
+      return 2;
+    case 0x33:
+      memory[vI] = *vX / 100 % 10;
+      memory[vI + 1] = *vX / 10 % 10;
+      memory[vI + 2] = *vX % 10;
+      return 2;
+    case 0x1E:
+      vI = *vX + vI;
+      return 2;
+    case 0x15:
+      DT = *vX;
+      return 2;
+    case 0x18:
+      ST = *vX;
+      return 2;
+    case 0x07:
+      *vX = DT;
+      return 2;
+    case 0x29:
+      vI = *vX * 5;
+      return 2;
+    case 0xA:
+      keyboardCopy = false;
+      for (int i = 0; i < 16; i++) {
+        if (keyboard[i]) {
+          if (keyboardCopy != keyboard[i]) {
+            *vX = i;
+            return 2;
+          }
+          keyboardCopy = keyboard[i];
+        }
+      }
+      return 0;
+    }
+  case 0xE000:
+    vX = &registers[secondNibble];
+    switch (byte2) {
+    case 0x9E:
+      return skipInstructionIf(keyboard[*vX] == true);
+    case 0xA1:
+      return skipInstructionIf(keyboard[*vX] == false);
+    }
+  default:
+    switch (cI) {
+    case 0x00E0:
+      for (int i = 0; i < 63; i++) {
+        for (int k = 0; k < 61; k++) {
+          screen[i][k] = false;
+        }
+      }
+      return 2;
+    case 0x00EE:
+      PC = stack[SP];
+      SP--;
+      return 2;
     default:
-	switch(cI){
-	case 0x00E0:
-	   for(int i=0; i < 63; i++){
-       	     for(int k=0; k < 61; k++){
-               screen[i][k] = false;
-		}
-	}
+      char fail[256];
+      sprintf(fail, "Unknown Opcode 0x%X", cI);
+      SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", fail, NULL);
+      exit(2);
       return 2;
-      case 0x00EE:
-     	 PC = stack[SP];
-      	 SP--;
-      return 2;
-      default:
-      	/*char fail[256];
-      	sprintf(fail, "Unknown Opcode 0x%X", cI);
-      	SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", fail, NULL);
-      	exit(2);*/
-      	return 2;
     }
   }
 }
 
-int skipInstructionIf(bool check){
-  if(check){
+int skipInstructionIf(bool check) {
+  if (check) {
     return 4;
-  }
-  else{
+  } else {
     return 2;
   }
 }
--- src/main.c
+++ src/main.c
@@ -7,162 +7,158 @@ bool keyboard[16];
 bool screen[64][32];
 bool quit = false;
 const char keymap[17] = "1234qwerasfzxcv";
-char* check;
+char *check;
 void callback(void *userdata, Uint8 *stream, int len);
-void squareWave(int pitch); 
+void squareWave(int pitch);
 void audioInit();
 char keyinput;
-static int freq = 440;  
+static int freq = 440;
 u_int8_t memory[4096] = {
-0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
-0x20, 0x60, 0x20, 0x20, 0x70, // 1
-0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
-0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
-0x90, 0x90, 0xF0, 0x10, 0x10, // 4
-0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
-0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
-0xF0, 0x10, 0x20, 0x40, 0x40, // 7
-0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
-0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
-0xF0, 0x90, 0xF0, 0x90, 0x90, // A
-0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
-0xF0, 0x80, 0x80, 0x80, 0xF0, // C
-0xE0, 0x90, 0x90, 0x90, 0xE0, // D
-0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
-0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80  // F
 };
 u_int8_t temp[ROM_SIZE];
 u_int16_t PC = 0x200;
-void init(char *file){
-    memset(registers, 0, sizeof(registers));
-    memset(keyboard, false, sizeof(keyboard));
-    FILE *rom;
-    size_t length;
-    rom = fopen(file, "r");
+void init(char *file) {
+  memset(registers, 0, sizeof(registers));
+  memset(keyboard, false, sizeof(keyboard));
+  FILE *rom;
+  size_t length;
+  rom = fopen(file, "r");
 
-    if (rom == NULL) {
-        perror("Cannot open");
-        exit(-1);
-    }
-
-    length = fread(temp, sizeof(u_int8_t), ROM_SIZE, rom);
-
-    if (length != ROM_SIZE) {
-        if (ferror(rom)) {
-            perror("Error reading file");
-            fclose(rom);
-            exit(2);
-        }
-    }
-
-    for (size_t i = 0; i < length; i++) {
-        memory[i + 512] = temp[i];
-    }
-    fclose(rom);
-//Temp
-//memory[0x1FF] = 1;
-
+  if (rom == NULL) {
+    perror("Cannot open");
+    exit(-1);
   }
-int main(int argc, char ** argv){
-    SDL_Event event;
-    SDL_Renderer *renderer;
-    SDL_Window *window;
-    if(argv[1] != NULL){
+
+  length = fread(temp, sizeof(u_int8_t), ROM_SIZE, rom);
+
+  if (length != ROM_SIZE) {
+    if (ferror(rom)) {
+      perror("Error reading file");
+      fclose(rom);
+      exit(2);
+    }
+  }
+
+  for (size_t i = 0; i < length; i++) {
+    memory[i + 512] = temp[i];
+  }
+  fclose(rom);
+  // Temp
+  // memory[0x1FF] = 1;
+}
+int main(int argc, char **argv) {
+  SDL_Event event;
+  SDL_Renderer *renderer;
+  SDL_Window *window;
+  if (argv[1] != NULL) {
     init(argv[1]);
-    }
-    else{
-        printf("Please enter a file name");
-        exit(1);
-    }
-    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
-    SDL_CreateWindowAndRenderer(pixelSize * 64, pixelSize * 32, 0, &window, &renderer);
-    audioInit();
-    while (!quit) {
+  } else {
+    printf("Please enter a file name");
+    exit(1);
+  }
+  SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
+  SDL_CreateWindowAndRenderer(pixelSize * 64, pixelSize * 32, 0, &window,
+                              &renderer);
+  audioInit();
+  while (!quit) {
     int m_startTime = SDL_GetTicks();
     SDL_RenderClear(renderer);
-    for (int x=0; x < 64; x++){
-        for (int y=0; y < 128; y++){
-            if(screen[x][y]){
-            createLargePixel(x, y, pixelSize, renderer);
-            }
+    for (int x = 0; x < 64; x++) {
+      for (int y = 0; y < 128; y++) {
+        if (screen[x][y]) {
+          createLargePixel(x, y, pixelSize, renderer);
         }
+      }
     }
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
     SDL_RenderPresent(renderer);
-    SDL_PollEvent(&event); 
-    switch(event.type){
+    SDL_PollEvent(&event);
+    switch (event.type) {
     case SDL_QUIT:
-	   quit = true;
+      quit = true;
     case SDL_KEYDOWN:
-	keyinput = event.key.keysym.sym;
-	check = strchr(keymap ,keyinput);
-	if (check){
-	keyboard[(int)(check - keymap)] = true;
-	}
-	break;
+      keyinput = event.key.keysym.sym;
+      check = strchr(keymap, keyinput);
+      if (check) {
+        keyboard[(int)(check - keymap)] = true;
+      }
+      break;
     case SDL_KEYUP:
-	keyinput = event.key.keysym.sym;
-	check = strchr(keymap,keyinput);
-	if (check){
-	keyboard[(int)(check - keymap)] = false;
-	}
-	break;
+      keyinput = event.key.keysym.sym;
+      check = strchr(keymap, keyinput);
+      if (check) {
+        keyboard[(int)(check - keymap)] = false;
+      }
+      break;
     }
-    //Run CPU Cycle
-    printf("PC: 0x%X| Adress at PC: 0x%X \n", PC, (memory[PC] << 8) | memory[PC + 1]);
+    // Run CPU Cycle
+    printf("PC: 0x%X| Adress at PC: 0x%X \n", PC,
+           (memory[PC] << 8) | memory[PC + 1]);
     PC = PC + cpuStep((memory[PC] << 8) | memory[PC + 1]);
-    //Buzzer
-    if(ST != 0){
-	squareWave(440);
+    // Buzzer
+    if (ST != 0) {
+      squareWave(440);
+    } else {
+      squareWave(1);
     }
-    else{
-	squareWave(1);
+    // FPS Limiting
+    if (1000 / FPS > SDL_GetTicks() - m_startTime) {
+      SDL_Delay((1000 / FPS) - (SDL_GetTicks() - m_startTime));
     }
-    //FPS Limiting
-      if (1000 / FPS > SDL_GetTicks() - m_startTime)
-        {
-            SDL_Delay((1000 / FPS) - (SDL_GetTicks() - m_startTime));
-        }
-    }
+  }
 exit:;
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    return EXIT_SUCCESS;
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+  return EXIT_SUCCESS;
 }
-void createLargePixel(int x, int y, int pixel_size, SDL_Renderer *renderer){
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_Rect rect = { x * pixel_size, y * pixel_size, pixel_size, pixel_size };
-    SDL_RenderFillRect(renderer, &rect);
+void createLargePixel(int x, int y, int pixel_size, SDL_Renderer *renderer) {
+  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+  SDL_Rect rect = {x * pixel_size, y * pixel_size, pixel_size, pixel_size};
+  SDL_RenderFillRect(renderer, &rect);
 }
-void audio_callback(void* userdata, Uint8* stream, int len) {
-    static bool high = true;
-    static int sample_count = 0;
-    Sint16* buffer = (Sint16*)stream;
-    int length = len / 2;  
-    for (int i = 0; i < length; ++i) {
-        if (sample_count >= SAMPLE_RATE / (freq * 2)) { 
-            high = !high;
-            sample_count = 0;
-        }
-
-        buffer[i] = high ? AMPLITUDE : -AMPLITUDE;
-        sample_count++;
+void audio_callback(void *userdata, Uint8 *stream, int len) {
+  static bool high = true;
+  static int sample_count = 0;
+  Sint16 *buffer = (Sint16 *)stream;
+  int length = len / 2;
+  for (int i = 0; i < length; ++i) {
+    if (sample_count >= SAMPLE_RATE / (freq * 2)) {
+      high = !high;
+      sample_count = 0;
     }
-}
-void audioInit(){
-    SDL_AudioSpec desired_spec;
-    SDL_zero(desired_spec);
-    desired_spec.freq = SAMPLE_RATE;
-    desired_spec.format = AUDIO_S16SYS;
-    desired_spec.channels = 1;
-    desired_spec.samples = 128;
-    desired_spec.callback = audio_callback;
 
-    SDL_AudioDeviceID device_id = SDL_OpenAudioDevice(NULL, 0, &desired_spec, NULL, 0);
-    SDL_PauseAudioDevice(device_id, 0);
-    
+    buffer[i] = high ? AMPLITUDE : -AMPLITUDE;
+    sample_count++;
+  }
 }
-void squareWave(int pitch) {
-    freq = pitch;
+void audioInit() {
+  SDL_AudioSpec desired_spec;
+  SDL_zero(desired_spec);
+  desired_spec.freq = SAMPLE_RATE;
+  desired_spec.format = AUDIO_S16SYS;
+  desired_spec.channels = 1;
+  desired_spec.samples = 128;
+  desired_spec.callback = audio_callback;
+
+  SDL_AudioDeviceID device_id =
+      SDL_OpenAudioDevice(NULL, 0, &desired_spec, NULL, 0);
+  SDL_PauseAudioDevice(device_id, 0);
 }
+void squareWave(int pitch) { freq = pitch; }
